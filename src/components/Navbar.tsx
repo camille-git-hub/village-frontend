bf640375313698fb485b8a67d54dd069caca2120
« Camille Rahout» <camille.rahout@gmail.com>
2026-04-07 14:01:28 +0200

    replace a href with Link elements

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,5 +1,6 @@
 import { useContext } from "react";
 import { AuthContext } from "../context/AuthContext.tsx";
+import { Link } from "react-router";
 
 
 const Navbar = () => {
@@ -15,10 +16,10 @@ const Navbar = () => {
         <div className="flex items-center justify-between">
             <div />
           <ul className="hidden md:flex gap-3 bg-gray-200 rounded px-2 py-1 text-sm">
-            <li><a href={isLoggedin ? "/listings" : "/login"} className="px-3 py-1 rounded">Listings</a></li>
-            <li><a href={isLoggedin ? "/explore" : "/login"} className="px-3 py-1 rounded">Explore</a></li>
-            <li><a href={isLoggedin ? "/connect" : "/login"} className="px-3 py-1 rounded">Connect</a></li>
-            <li><a href={isLoggedin ? "/profile" : "/login"} className="px-3 py-1 rounded">Profile</a></li>
+            <li><Link to={isLoggedin ? "/listings" : "/login"} className="px-3 py-1 rounded">Listings</Link></li>
+            <li><Link to={isLoggedin ? "/explore" : "/login"} className="px-3 py-1 rounded">Explore</Link></li>
+            <li><Link to={isLoggedin ? "/connect" : "/login"} className="px-3 py-1 rounded">Connect</Link></li>
+            <li><Link to={isLoggedin ? "/profile" : "/login"} className="px-3 py-1 rounded">Profile</Link></li>
           </ul>
         </div>
         <div><a href={isLoggedin ? "/listings" : "/"} className="mt-4 text-6xl font-semibold">Village</a></div>
