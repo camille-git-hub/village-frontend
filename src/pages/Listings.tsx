2c68d93415179a2b50036efaf0222aba8518b4a4
« Camille Rahout» <camille.rahout@gmail.com>
2026-04-15 14:38:52 +0200

    fixed build error

--- src/pages/Listings.tsx
+++ src/pages/Listings.tsx
@@ -2,7 +2,6 @@ import { useEffect, useState } from "react";
 import { useNavigate } from "react-router";
 import type { Listing } from "../types/listing.ts";
 import { ListingCard } from "../components/ListingCard.tsx";
-import { HAMBURG_NEIGHBORHOODS } from "../utils/neightborhoods.ts";
 
 export const ListingsPage = () => {
     const [listings, setListings] = useState<Listing[]>([]);
